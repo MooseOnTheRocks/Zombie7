48cf9271b0d4721b296d61d521857a108ed0b38d
birdsdrib <ricky.jenkins@live.com>
2023-11-16 16:21:46 -0800

    fixed redundancy

--- src/main/java/dev/foltz/item/gun/rifle/MusketRifleItem.java
+++ src/main/java/dev/foltz/item/gun/rifle/MusketRifleItem.java
@@ -25,7 +25,7 @@ public class MusketRifleItem extends GunStagedItem<MusketRifleItem> {
                         .onPressShoot(tryShootOrReload(STAGE_COCKING, STAGE_RELOADING))
                         .onPressReload(tryReload(STAGE_RELOADING)),
 
-                STAGE_RELOADING, new GunStageBuilder<>(ticksFromSeconds(5f))
+                STAGE_RELOADING, new GunStageBuilder<>(ticksFromSeconds(5.0f))
                         .barColor(stack -> YELLOW)
                         .barProgress(stack -> ((GunStagedItem<?>) stack.getItem()).getStageTicks(stack) / (float) ((GunStagedItem<?>) stack.getItem()).getMaxStageTicks(stack))
                         .onInitDo(view -> {
